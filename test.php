b0a4a4d7deb60b4e9ca57d5d57ce1006ba9b1db4
Bartlomiej Zalas <bartekzalas@gmail.com>
2017-03-07 22:36:54 +0100

    ssl test

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
 echo isset($_SERVER['HTTPS']) . ":". $_SERVER['HTTPS'] . "<br>";
-echo isset($_SERVER['SSL']) . ":". $_SERVER['HTTPS'] . "<br>";
-echo isset($_SERVER['REDIRECT_HTTPS']) . ":". $_SERVER['HTTPS'] . "<br>";
-echo isset($_SERVER['HTTP_SSL']) . ":". $_SERVER['HTTPS'] . "<br>";
-echo isset($_SERVER['HTTP_X_FORWARDED_PROTO']) . ":". $_SERVER['HTTPS'] . "<br>";
+echo isset($_SERVER['SSL']) . ":". $_SERVER['SSL'] . "<br>";
+echo isset($_SERVER['REDIRECT_HTTPS']) . ":". $_SERVER['REDIRECT_HTTPS'] . "<br>";
+echo isset($_SERVER['HTTP_SSL']) . ":". $_SERVER['HTTP_SSL'] . "<br>";
+echo isset($_SERVER['HTTP_X_FORWARDED_PROTO']) . ":". $_SERVER['HTTP_X_FORWARDED_PROTO'] . "<br>";
 
 ?>
