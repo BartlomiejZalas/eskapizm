29b0de8ad86f478ff66ca6d773b53027412ff8bc
Bartlomiej Zalas <bartekzalas@gmail.com>
2017-03-07 22:31:47 +0100

    ssl test

---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+echo isset($_SERVER['HTTPS']) + ":"+ $_SERVER['HTTPS'] + "<br>";
+echo isset($_SERVER['SSL']) + ":"+ $_SERVER['HTTPS'] + "<br>";
+echo isset($_SERVER['REDIRECT_HTTPS']) + ":"+ $_SERVER['HTTPS'] + "<br>";
+echo isset($_SERVER['HTTP_SSL']) + ":"+ $_SERVER['HTTPS'] + "<br>";
+echo isset($_SERVER['HTTP_X_FORWARDED_PROTO']) + ":"+ $_SERVER['HTTPS'] + "<br>";
+
+?>
